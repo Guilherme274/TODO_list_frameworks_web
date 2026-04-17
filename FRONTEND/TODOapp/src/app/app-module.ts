6b6665d5c84077e43d58ee67bd57c2a0813415cd
Guilherme Paixão <paixaoguilherme79@gmail.com>
2026-04-17 20:39:50 -0300

    :sparkles: feat: Update dependencies and implement CRUD operations with HTTP client

--- FRONTEND/TODOapp/src/app/app-module.ts
+++ FRONTEND/TODOapp/src/app/app-module.ts
@@ -1,13 +1,23 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// Importe o provideHttpClient e o withFetch
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { App } from './app';
 import { Item } from './item/item';
 
 @NgModule({
   declarations: [App, Item],
-  imports: [BrowserModule],
-  providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
+  imports: [
+    BrowserModule
+    // Remova o HttpClientModule daqui para evitar conflitos
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(), 
+    provideClientHydration(withEventReplay()),
+    // Adicione o provider do HttpClient com Fetch aqui
+    provideHttpClient(withFetch())
+  ],
   bootstrap: [App],
 })
 export class AppModule {}
